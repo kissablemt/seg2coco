5f18015f89ab7e88fb2e220d7bb967a5c694db67
wzt <wzt@wztdeMacBook-Pro-2.local>
2021-05-19 22:20:37 +0800

    Add `categories` recognition

--- seg2coco.py
+++ seg2coco.py
@@ -19,22 +19,33 @@ def test_time(start_or_end):
     else:
         test_time.start = time.time()
 
-def gen_anno(seg_img, image_id):
-    if len(seg_img.shape) != 2:
-        seg_img = cv2.cvtColor(seg_img, cv2.COLOR_BGR2GRAY)
-    height, width = seg_img.shape[:2]
+def gen_anno(inst_img, sem_img, image_id):
+    try:
+        if inst_img.shape[:2] != sem_img.shape[:2]:
+            raise Exception
+    except:
+        return None
+
+    if len(inst_img.shape) != 2:
+        inst_img = cv2.cvtColor(inst_img, cv2.COLOR_BGR2GRAY)
+    if len(sem_img.shape) != 2:
+        sem_img = cv2.cvtColor(sem_img, cv2.COLOR_BGR2GRAY)
+        
+    height, width = inst_img.shape[:2]
     
     sub_masks = {}
+    sub_masks_cls = {}
     for y in range(height):
         for x in range(width):
-            pixel = seg_img[y, x]
+            pixel = inst_img[y, x]
             if pixel != 0:
                 sub_mask = sub_masks.get(pixel, None)
                 if sub_mask is None:
                     sub_masks[pixel] = np.zeros((height, width), dtype=np.uint8)
+                    sub_masks_cls[pixel] = int(sem_img[y, x])
                 sub_masks[pixel][y, x] = 255
     annotations = []
-    for binary_mask in sub_masks.values():
+    for pixel, binary_mask in sub_masks.items():
         fortran_ground_truth_binary_mask = np.asfortranarray(binary_mask)
         encoded_ground_truth = mask.encode(fortran_ground_truth_binary_mask)
         ground_truth_area = mask.area(encoded_ground_truth)
@@ -53,7 +64,7 @@ def gen_anno(seg_img, image_id):
             "iscrowd": 0,
             "image_id": image_id,
             "bbox": ground_truth_bounding_box.tolist(),
-            "category_id": 1,
+            "category_id": sub_masks_cls[pixel],
             "id": int(time.time() * 1000),
         }
         
@@ -68,17 +79,18 @@ def main():
         "annotations": [],
         "categories": [],
     }
-    seg_dir = "COCO/tmp/inst"
-    listdir = os.listdir(seg_dir)
+    inst_dir = "COCO/tmp/inst"
+    listdir = os.listdir(inst_dir)
     listdir.sort(key=lambda x: int(x.split('.')[0]))
 
+    sem_dir = "COCO/tmp/sem"
     i = 0
     for filename in listdir:
-        filepath = os.path.join(seg_dir, filename)
-        # print(filepath)
         try:
-            seg_img = cv2.imread(filepath)
-            height, width = seg_img.shape[:2]
+            # Their file names should be same.
+            inst_img = cv2.imread(os.path.join(inst_dir, filename))
+            sem_img = cv2.imread(os.path.join(sem_dir, filename))
+            height, width = inst_img.shape[:2]
 
             image_id = int(os.path.basename(filename).split('.')[0])
             image = {
@@ -88,7 +100,10 @@ def main():
                 "id": image_id,
             }
 
-            annotation = gen_anno(seg_img, image_id)
+            annotation = gen_anno(inst_img, sem_img, image_id)
+            if annotation is None:
+                continue
+
             coco_json["images"].append(image)
             coco_json["annotations"].extend(annotation)
 
@@ -100,9 +115,15 @@ def main():
             print(e)
 
     coco_json["categories"] = [
-        {"id": 1, "name": "target", "supercategory": "target"}
+        {"id": 1, "name": "cube", "supercategory": "object"},
+        {"id": 2, "name": "monkey", "supercategory": "object"},
     ] 
-    with open("COCO/annotations/{}.json".format(int(time.time() * 1000)), "w") as f:
+
+    json_file = "COCO/annotations/{}.json".format(int(time.time() * 1000))
+    print(json.dumps(coco_json))
+    
+    os.makedirs(os.path.dirname(json_file), exist_ok=True)
+    with open(json_file, "w") as f:
         f.write(json.dumps(coco_json))
 
     
